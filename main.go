e0408a09bb779c6b1576a872aba04b39caba5787
mind1949 <lianjie1949@gmail.com>
2019-06-20 18:52:42 +0800

    feat: 添加session验证功能

--- main.go
+++ main.go
@@ -1,11 +1,35 @@
 package main
 
 import (
+	"github.com/go-redis/redis"
+	"log"
 	"net/http"
 
 	"github.com/labstack/echo"
 	"github.com/labstack/echo/middleware"
 )
+
+const (
+	redisAddr     = "127.0.0.1:6379"
+	redisPassword = ""
+	redisDB       = 1
+	sidKey        = "else.sid"
+
+	mysqlAddr = "127.0.0.1:3306"
+)
+
+var (
+	redisClient *redis.Client
+)
+
+// 完成redis与mysql连接的初始化
+func init() {
+	redisClient = defaultRedisClient()
+}
+
+// ------------------
+// ---- server ------
+// ------------------
 
 func main() {
 	// 实例化一个Echo类型
@@ -13,11 +37,59 @@ func main() {
 	// 添加中间件
 	e.Use(middleware.Logger())
 	// 添加路由
-	e.GET("/", hello)
+	e.GET("/helloredis", helloredis, middlewareValidateSession())
+	e.GET("/hellomysql", hellomysql)
 	// 启动服务器
 	e.Logger.Fatal(e.Start(":1323"))
 }
 
-func hello(c echo.Context) error {
-	return c.JSON(http.StatusOK, "Hello, World !")
+// --------------------
+// ----- database------
+// --------------------
+
+func defaultRedisClient() *redis.Client {
+	client := redis.NewClient(&redis.Options{
+		Addr:     redisAddr,
+		Password: redisPassword,
+		DB:       redisDB,
+	})
+	pong, err := client.Ping().Result()
+	log.Println(pong, err)
+	return client
+}
+
+// --------------------
+// ----- handler ------
+// --------------------
+
+func helloredis(c echo.Context) error {
+	return c.JSON(http.StatusOK, "Hello, redis")
+}
+
+func hellomysql(c echo.Context) error {
+	return c.JSON(http.StatusOK, "Hello mysql")
+}
+
+// --------------------
+// ---- middleware ----
+// --------------------
+
+// 中间件: 进行session验证
+func middlewareValidateSession() echo.MiddlewareFunc {
+	return func(next echo.HandlerFunc) echo.HandlerFunc {
+		return func(c echo.Context) error {
+			// 尝试获取session id
+			sidCookie, err := c.Cookie(sidKey)
+			if err != nil {
+				return c.JSON(http.StatusForbidden, "no session id in cookie !")
+			}
+			// 尝试获取session
+			session, err := redisClient.Do("Get", sidCookie.Value).String()
+			if err != nil {
+				log.Printf("err: redisClient.Do(%v) session=%q err=%v \n", sidCookie.Value, session, err)
+				return c.JSON(http.StatusForbidden, "session verification failed !")
+			}
+			return next(c)
+		}
+	}
 }
